d536914bbc72e2f6e79c5c661f92900e7d8294a1
Krystian Fras <krystian@fras.me>
2025-04-03 20:34:16 +0200

    Fix providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,36 @@ import "./App.css";
 import { AppsSidebar } from "./packages/ui-kit/AppsSidebar";
 import { Header } from "./components/Header";
 import { IndexPage } from "./pages";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div className="flex h-full grow">
-        <AppsSidebar />
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex h-full grow">
+          <AppsSidebar />
 
-        <div className="w-full bg-background p-4 ">
-          <Routes>
-            <Route index element={<IndexPage />} />
-          </Routes>
+          <div className="w-full bg-background p-4 ">
+            <Routes>
+              <Route index element={<IndexPage />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
