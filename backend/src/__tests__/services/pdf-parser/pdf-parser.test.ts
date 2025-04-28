88e69893af2b6e762163b8989c0ee216c6ac87ef
Piotr Wojciechowski <piotr.wojciechowski@tooploox.com>
2025-04-28 22:14:17 +0200

    fix: tests

--- backend/src/__tests__/services/pdf-parser/pdf-parser.test.ts
+++ backend/src/__tests__/services/pdf-parser/pdf-parser.test.ts
@@ -3,7 +3,7 @@ import { PDFParserService } from "../../../services/pdf-parser.js";
 
 const expectedSections = [
   {
-    title: null,
+    title: "Abstract",
     text: "JOIN-ACCUMULATE MACHINE: A MOSTLY-COHERENT TRUSTLESS SUPERCOMPUTER DRAFT 0.6.4 - March 18, 2025 DR. GAVIN WOOD FOUNDER, POLKADOT & ETHEREUM GAVIN@PARITY.IO Abstract. We present a comprehensive and formal definition of Jam, a protocol combining elements of both Polkadot and Ethereum. In a single coherent model, Jam provides a global singleton permissionless object environment—much like the smart-contract environment pioneered by Ethereum—paired with secure sideband computation parallelized over a scalable node network, a proposition pioneered by Polkadot. Jam introduces a decentralized hybrid system offering smart-contract functionality structured around a secure and scalable in-core/on-chain dualism. While the smart-contract functionality implies some similarities with Ethereum’s paradigm, the overall model of the service offered is driven largely by underlying architecture of Polkadot. Jam is permissionless in nature, allowing anyone to deploy code as a service on it for a fee commensurate with the resources this code utilizes and to induce execution of this code through the procurement and allocation of core-time, a metric of resilient and ubiquitous computation, somewhat similar to the purchasing of gas in Ethereum. We already envision a Polkadot-compatible CoreChains service.",
     subsections: [],
   },
