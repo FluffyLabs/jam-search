09e4f49163ce1a1aae581f3aa82e2960b4df3a10
Krystian Fras <krystian@all-turtles.com>
2025-05-03 16:19:10 +0200

    Fix script run

---
+++ backend/src/scripts/fillArchivedMessageForNDays.ts
@@ -0,0 +1,28 @@
+import { subDays, format } from "date-fns";
+import { fillArchivedMessages } from "./fillArchivedMessages.js";
+
+const N_DAYS = 1000;
+// For running directly:
+if (process.argv.length >= 4) {
+  // Case: From and To dates provided as arguments
+  const fromDate = process.argv[2];
+  const toDate = process.argv[3];
+  fillArchivedMessages(fromDate, toDate).finally(() => {
+    process.exit(0);
+  });
+} else {
+  // Calculate dates based on daysBack or default to 1000 days
+  const daysBack = process.argv[2]
+    ? Number.parseInt(process.argv[2], 10)
+    : N_DAYS;
+  const today = new Date();
+  const pastDate = subDays(today, daysBack);
+
+  const toDate = format(today, "yyyy-MM-dd");
+  const fromDate = format(pastDate, "yyyy-MM-dd");
+
+  console.log(`Using daysBack=${daysBack} to calculate date range`);
+  fillArchivedMessages(fromDate, toDate).finally(() => {
+    process.exit(0);
+  });
+}
--- backend/src/scripts/fillArchivedMessages.ts
+++ backend/src/scripts/fillArchivedMessages.ts
@@ -1,4 +1,4 @@
-import { format, isValid, parse, subDays } from "date-fns";
+import { isValid, parse } from "date-fns";
 import { db } from "../db/db.js";
 import { fetchArchivedMessages } from "../services/archive.js";
 import { MessagesLogger } from "../services/logger.js";
@@ -86,29 +86,4 @@ export async function fillArchivedMessages(fromDate: string, toDate: string) {
   } catch (error) {
     console.error("Error fetching and inserting historical messages:", error);
   }
-}
-
-// For running directly:
-if (process.argv.length >= 4) {
-  // Case: From and To dates provided as arguments
-  const fromDate = process.argv[2];
-  const toDate = process.argv[3];
-  fillArchivedMessages(fromDate, toDate).finally(() => {
-    process.exit(0);
-  });
-} else {
-  // Calculate dates based on daysBack or default to 1000 days
-  const daysBack = process.argv[2]
-    ? Number.parseInt(process.argv[2], 10)
-    : 1000;
-  const today = new Date();
-  const pastDate = subDays(today, daysBack);
-
-  const toDate = format(today, "yyyy-MM-dd");
-  const fromDate = format(pastDate, "yyyy-MM-dd");
-
-  console.log(`Using daysBack=${daysBack} to calculate date range`);
-  fillArchivedMessages(fromDate, toDate).finally(() => {
-    process.exit(0);
-  });
 }
