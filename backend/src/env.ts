802f2677f579bea7e0d48c95c0903c8ab404334e
Krystian Fras <krystian@all-turtles.com>
2025-05-03 16:02:53 +0200

    Fix tests

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -4,12 +4,6 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   HOMESERVER_URL: z.string().url(),
-  ACCESS_TOKEN: z.string().min(1),
-  USER_ID: z.string().min(1),
-  ROOM_IDS: z
-    .string()
-    .min(1)
-    .transform((val) => val.split(";")),
   POSTGRES_URL: z.string().url(),
   PORT: z.coerce.number().default(3000),
 });
