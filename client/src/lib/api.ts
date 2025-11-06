2d4d4b8e45a2f490c321f653732ac566570aadd9
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-11-06 17:09:07 +0100

    Point client to the new backend (#132)
    * Point client to new backend.
    
    * Trigger build.

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -3,9 +3,7 @@
  */
 
 // Base URL for API requests
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "https://matrix-bot-jam-5e043e51602b.herokuapp.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://search-api.fluffylabs.dev";
 
 /**
  * Type definitions for API responses
