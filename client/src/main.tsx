8fde0f2aece67bd0c361d8b3a7086a86a1ad49b9
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-11-10 22:28:18 +0100

    Bump shared-ui and reduce bundle size. (#160)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { scan } from "react-scan"; // must be imported before React and React DOM
 import "@fluffylabs/shared-ui/style.css";
 import "./index.css";
-
-scan({
-  enabled: process.env.NODE_ENV !== "production",
-});
 
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
