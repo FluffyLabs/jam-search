b352b3b1919f3009c0712ecff6aa9e37bbcb8739
Tomasz Drwięga <tomusdrw@gmail.com>
2026-04-20 23:37:19 +0200

    Add CODE_REPOSITORIES config for code ingestion

---
+++ shared/code.ts
@@ -0,0 +1,31 @@
+import { Source } from "./sources.js";
+
+export type CodeRepository = {
+  source: Source;
+  dbId: string;
+  owner: string;
+  repo: string;
+  /** Override default branch (auto-detected from clone if omitted). */
+  defaultBranch?: string;
+};
+
+export const CODE_REPOSITORIES: CodeRepository[] = [
+  {
+    source: Source.GithubFluffyLabsTypeberry,
+    dbId: "github.com/fluffylabs/typeberry",
+    owner: "fluffylabs",
+    repo: "typeberry",
+  },
+  {
+    source: Source.GithubTomusdrwAsLan,
+    dbId: "github.com/tomusdrw/as-lan",
+    owner: "tomusdrw",
+    repo: "as-lan",
+  },
+  {
+    source: Source.GithubTomusdrwAnanAs,
+    dbId: "github.com/tomusdrw/anan-as",
+    owner: "tomusdrw",
+    repo: "anan-as",
+  },
+];
--- shared/index.ts
+++ shared/index.ts
@@ -1,3 +1,4 @@
+export * as code from "./code.js";
 export * as discord from "./discord.js";
 export * as github from "./github.js";
 export * as matrix from "./matrix.js";
